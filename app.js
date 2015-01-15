fc4059b7f1b5752435812f7428a7fb64ef546735
Brian Powell <brian@straphq.com>
2015-01-15 14:17:39 -0500

    landing page version

--- app.js
+++ app.js
@@ -17,24 +17,12 @@ exports.init = function(config) {
     app.configure(function(){
         app.set('views', __dirname + '/views');
         app.set('view engine', 'ejs');
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
         app.use(express.static(__dirname + '/static'));
         app.use(app.router);
         app.enable("jsonp callback");
     });
 
     app.engine('ejs', engine);
-
-    app.configure('development', function(){
-       app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-        // app.use(express.logger({ format: ':method :url' }));
-    });
-
-    app.configure('production', function(){
-       app.use(express.errorHandler()); 
-    });
-
 
     app.use( function(err, req, res, next) {
         res.render('500.ejs', { locals: { error: err }, status: 500 });
