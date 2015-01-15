cfccb0ee4d318e0a2ab815d6f767ac3e14b80262
Brian Powell <brian@straphq.com>
2015-01-15 15:57:25 -0500

    initial markdown loading stuff

--- server.js
+++ server.js
@@ -4,26 +4,7 @@ var pkg = require("./package.json"),
 	app = require('./app').init(pkg.server);
 
 //Other Pages
-app.get('/:page', function(req,res){
-
-	//console.log("Page ID:", req.params.page);
-	var page = req.params.page || "",
-		temp = {body: "home.ejs", page: page, config: pkg.config};
-	switch(page){
-		case "docpsection":
-			temp.body = page+".ejs"
-			res.render('layout.ejs', temp);
-		break;
-	}
-});
-
-//home Page
-app.get('/', function(req,res){
-    res.render('layout.ejs', {body: "home.ejs", page: "home", config: pkg.config});
-});
-
-/* The 404 Route (ALWAYS Keep this as the last route) */
-app.get('/*', function(req, res){
-    res.render('404.ejs');
+app.get('/*', function(req,res){
+	res.render('layout.ejs');
 });
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,27 @@
 //Scripts for Main page
 
+var url = {};
+
 //Get everything rolling
 $( window ).ready(kickstart);
 
 //Main function for homepage
 function kickstart() {
-	console.log("We are kickstarting "+page);
 
+    url = $.url();
+
+	console.log("We are kickstarting "+ url.attr('path') );
+    load_page("home");
+}
+
+function load_page(page) {
+    $.ajax({
+        url: "markdown/"+page+".md",
+        success: function(data) {
+            $("#content").html( marked(data) );
+            console.log("loaded page "+page);
+        }
+    });
 }
 
 
