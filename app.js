b3bd38991c70c222a800d378309465aeb385f608
Steve Caldwell <steve@crosstek.net>
2015-01-15 17:54:06 -0500

    move all routes to app.js for sanity, also reformatted code which ballooned the diff

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 /* 
  * app.js
- * 
+ *
  * Our base app code, including Express configs
  */
 
-var fs = require('fs')
-  , express = require('express')
-  , engine = require('ejs-locals')
-  , app = express()
-  , credentials = {};
+var fs = require('fs'),
+    express = require('express'),
+    engine = require('ejs-locals'),
+    app = express(),
+    credentials = {};
 
 exports.init = function(config) {
 
-    app.configure(function(){
+    app.configure(function() {
         app.set('views', __dirname + '/views');
         app.set('view engine', 'ejs');
         app.use(express.static(__dirname + '/static'));
@@ -21,22 +21,28 @@ exports.init = function(config) {
     });
 
     app.engine('ejs', engine);
-   
 
 
-    // app.get('/*', function(req,res){
-    //   res.render('layout.ejs');
-    // });
 
-    app.get('/', function(req,res){
-      res.render('home.ejs');
-    }); 
-
-
-    app.use( function(err, req, res, next) {
-        res.render('500.ejs', { locals: { error: err }, status: 500 });
+    app.get('/', function(req, res) {
+        res.render('home.ejs');
     });
-    
+
+    //Other Pages
+    app.get('*', function(req, res) {
+        res.render('layout.ejs');
+    });
+
+
+    app.use(function(err, req, res, next) {
+        res.render('500.ejs', {
+            locals: {
+                error: err
+            },
+            status: 500
+        });
+    });
+
     server = app.listen(process.env.PORT || config.port);
 
     console.log("Listening on port %d in %s mode", process.env.PORT || config.port, app.settings.env);
